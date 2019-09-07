48653152f67ac09e98ad8342431bc334d41bb670
Marta Budnik <mbudnikm97@gmail.com>
2019-09-07 23:26:45 +0200

    fetchAll, edit product

--- Using Mongo/models/product.js
+++ Using Mongo/models/product.js
@@ -1,16 +1,55 @@
+const mongodb = require('mongodb')
 const getDb = require('../util/database').getDb
+
 class Product {
-  constructor(title, price, description, imageUrl) {
+  constructor(title, price, description, imageUrl, id) {
     this.title = title;
     this.price = price
     this.description = description
     this.imageUrl = imageUrl
+    this._id = new mongodb.ObjectId(id)
   }
 
   save() {
     const db = getDb()
-    db.collection('products').insertOne(this)
-      .then(result => console.log(result))
+    let dbOperation
+    if(this._id) {
+      dbOperation = db.collection('products')
+        .updateOne({ _id: this._id }, { $set: this })
+    } else {
+      dbOperation = db.collection('products')
+        .insertOne(this)
+    }
+
+    return dbOperation
+      .then(product => {
+        console.log(product)
+        return product
+      })
+      .catch(err => console.log(err))
+  }
+
+  static fetchAll() {
+    const db = getDb()
+    return db.collection('products')
+      .find()
+      .toArray()
+      .then(products => {
+        console.log(products)
+        return products
+      })
+      .catch(err => console.log(err))
+  }
+
+  static findById(prodId) {
+    const db = getDb()
+    return db.collection('products')
+      .find({_id: new mongodb.ObjectId(prodId)})
+      .next()
+      .then(product => {
+        console.log(product)
+        return product
+      })
       .catch(err => console.log(err))
   }
 }
