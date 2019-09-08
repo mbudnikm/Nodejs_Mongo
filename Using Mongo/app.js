4863c2530f04ebdf1695d1f5335ec3da9de44862
Marta Budnik <mbudnikm97@gmail.com>
2019-09-08 18:22:03 +0200

    add to cart

--- Using Mongo/app.js
+++ Using Mongo/app.js
@@ -4,8 +4,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-
 const mongoConnect = require('./util/database').mongoConnect
+const User = require('./models/user')
 
 const app = express();
 
@@ -19,7 +19,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  next()
+  User.findById('5d74bbf330ea8f098cdb8089')
+  .then(user => {
+    req.user = new User(user.name, user.email, user.cart, user._id)
+    next()
+  })
+  .catch(err => console.log(err))
 });
 
 app.use('/admin', adminRoutes);
