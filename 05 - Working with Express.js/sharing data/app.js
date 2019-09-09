dd67aa8b479bf0711250759092638917e914ffe0
mbudnikm <mbudnikm97@gmail.com>
2019-09-09 07:46:57 +0200

    module 5

---
+++ 05 - Working with Express.js/sharing data/app.js
@@ -0,0 +1,30 @@
+const http = require('http');
+
+const express = require('express');
+const bodyParser = require('body-parser')
+
+const app = express();
+
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use("/", (req, res, next) => {
+    console.log('This always runs')
+    next()
+})
+
+app.use("/add-product", (req, res, next) => {
+    console.log('In the other middleware')
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
+})
+
+app.post("/product", (req, res, next) => {
+    console.log(req.body)
+    res.redirect("/")
+})
+
+app.use("/", (req, res, next) => {
+    res.send('<h4>Hello from Express.js!</h4>')
+})
+
+app.listen(3000);
